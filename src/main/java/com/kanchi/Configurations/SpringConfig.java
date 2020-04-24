33589ae65dbcb7eead4abfda6ccee5466c27e704
Kanchana Nadeeshani <nadeeshanikj@gmail.com>
2020-04-24 15:49:29 +0530

    initial commit

---
+++ src/main/java/com/kanchi/Configurations/SpringConfig.java
@@ -0,0 +1,37 @@
+package com.kanchi.Configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.kanchi")
+//@PropertySource(value = {"classpath:application.properties"})
+public class SpringConfig extends WebMvcConfigurerAdapter {
+
+    @Bean(name = "viewResolver")
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        System.out.println("view resolver");
+        return viewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
